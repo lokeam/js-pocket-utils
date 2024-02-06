57352d9fe74299c7e8a04b52895971511279db3f
Ahn Ming Loke <lokeahnming@gmail.com>
2024-02-05 21:42:06 -0500

    practice: experimenting with streams, piping and performance

--- node/labs/server/app.js
+++ node/labs/server/app.js
@@ -4,12 +4,7 @@ var fs = require('fs');
 http.createServer(function(request, response) {
   response.writeHead(200, { 'Content-Type': 'text/html' });
 
-  // conversion to utf8 string needed for template replacement
-  var html = fs.readFileSync(__dirname + '/index.html', 'utf8');
-
-  var message = 'よろしく!';
-  html = html.replace('{message}', message)
-
-  response.end(html);
+  // create readable file stream, buffer then pipe to response
+  fs.createReadStream(__dirname + '/index.html', 'utf8').pipe(response);
 
 }).listen(1337, '127.0.0.1');
