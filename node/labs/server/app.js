566b41ab4623750e4fe009c697dd826ad8ba2093
Ahn Ming Loke <lokeahnming@gmail.com>
2024-02-05 21:36:50 -0500

    practice: html templating lab

--- node/labs/server/app.js
+++ node/labs/server/app.js
@@ -1,7 +1,15 @@
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function(request, response) {
-  response.writehead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello world\n');
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+
+  // conversion to utf8 string needed for template replacement
+  var html = fs.readFileSync(__dirname + '/index.html', 'utf8');
+
+  var message = 'よろしく!';
+  html = html.replace('{message}', message)
+
+  response.end(html);
 
 }).listen(1337, '127.0.0.1');
