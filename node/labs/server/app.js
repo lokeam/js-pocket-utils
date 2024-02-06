7dbdae65a845810222d5432f6650c62ead0d6d8e
Ahn Ming Loke <lokeahnming@gmail.com>
2024-02-05 21:59:55 -0500

    practice: observing manual http routing under the hood

--- node/labs/server/app.js
+++ node/labs/server/app.js
@@ -2,9 +2,23 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function(request, response) {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
 
-  // create readable file stream, buffer then pipe to response
-  fs.createReadStream(__dirname + '/index.html', 'utf8').pipe(response);
+  // manually route and create readable streams or write to head
+  if (request.url === '/') {
+    fs.createReadStream(__dirname + '/index.html', 'utf8').pipe(response);
+
+  } else if (request.url === '/api') {
+    response.writeHead(200, {'Content-Type': 'application/json'});
+
+    var obj = {
+      firstname: 'John',
+      lastname: 'Wick'
+    };
+
+    response.end(JSON.stringify(obj));
+  } else {
+    response.writeHead(404);
+    response.end();
+  }
 
 }).listen(1337, '127.0.0.1');
